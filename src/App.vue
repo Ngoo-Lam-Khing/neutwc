1a1810c1bfd99d031645a5131c6baa17390c9a09
wuben <wuben@garena.com>
2025-08-06 01:30:07 +0800

    feat: Add heart shape

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,23 @@
 import { computed, ref, watch } from "vue";
 import Square from "./components/Square.vue";
 
+// Reactive state for grid size
 const count = ref(1);
 const square = computed(() => count.value * count.value);
 function setGridSize(size) {
   count.value = size;
 }
 
+// Mode types for grid behavior
 const modeTypes = {
   all: "all",
   random: "random",
 };
-const mode = ref(modeTypes.all);
+const curMode = ref(modeTypes.all);
 function setMode(event) {
-  mode.value = event.target.value;
+  curMode.value = event.target.value;
 }
-
+// Set to track active squares in random mode
 const indexSet = ref(new Set());
 function random() {
   indexSet.value = new Set();
@@ -25,48 +27,65 @@ function random() {
     indexSet.value.add(Math.floor(Math.random() * square));
   }
 }
-
-watch([count, mode], ([newCount, newMode]) => {
-  if (newMode === modeTypes.random && newCount > 1) {
+// Watch for changes in count and curMode to update indexSet
+watch([count, curMode], ([newCount, newCurMode]) => {
+  if (newCurMode === modeTypes.random && newCount > 1) {
     random();
   } else {
     indexSet.value.clear();
   }
 });
+// Shape types for square component
+const shapeTypes = {
+  square: "square",
+  heart: "heart",
+};
+const curShape = ref(shapeTypes.square);
+function setShape(event) {
+  curShape.value = event.target.value;
+}
 </script>
 
 <template>
-  <button type="button" @click="setGridSize(1)">1 x 1</button>
-  <button type="button" @click="setGridSize(3)">3 x 3</button>
-  <button type="button" @click="setGridSize(5)">5 x 5</button>
-  <button type="button" @click="setGridSize(10)">10 x 10</button>
-  <input
-    type="radio"
-    name="mode"
-    id="all"
-    :value="modeTypes.all"
-    @change="setMode"
-    :checked="mode === modeTypes.all"
-  />
-  <label for="all">All</label>
-  <input
-    type="radio"
-    name="mode"
-    id="random"
-    :value="modeTypes.random"
-    @change="setMode"
-    :checked="mode === modeTypes.random"
-  />
-  <label for="random">Random</label>
+  <button
+    type="button"
+    v-for="size in [1, 3, 5, 10]"
+    @click="setGridSize(size)"
+  >
+    {{ size }} x {{ size }}
+  </button>
+  <label v-for="mode in modeTypes" :for="mode">
+    <input
+      type="radio"
+      name="mode"
+      :id="mode"
+      :value="mode"
+      @change="setMode"
+      :checked="mode === curMode"
+    />
+    {{ mode }}
+  </label>
+  <label v-for="shape in shapeTypes" :for="shape">
+    <input
+      type="radio"
+      name="shape"
+      :id="shape"
+      :value="shape"
+      @change="setShape"
+      :checked="shape === curShape"
+    />
+    {{ shape }}
+  </label>
   <section class="container">
     <Square
       v-for="(_i, index) in square"
       :key="Math.random()"
-      :class="
-        count === 1 || mode === modeTypes.all || indexSet.has(index)
+      :class="[
+        count === 1 || curMode === modeTypes.all || indexSet.has(index)
           ? 'active'
-          : 'inactive'
-      "
+          : 'inactive',
+        curShape === shapeTypes.heart ? 'square--heart' : '',
+      ]"
     />
   </section>
 </template>
--- src/components/Square.vue
+++ src/components/Square.vue
@@ -1,16 +1,7 @@
 <template>
   <div class="square"></div>
 </template>
-
-<script setup></script>
-
 <style scoped lang="scss">
-@keyframes opacityChange {
-  to {
-    --opacity: 1;
-  }
-}
-
 @property --angle {
   syntax: "<angle>";
   initial-value: 0deg;
@@ -27,27 +18,56 @@
 .square {
   width: 100%;
   height: 100%;
-  border: 0.02rem solid transparent;
-  border-radius: 0.1rem;
+  border: 0.01rem solid transparent;
+  border-radius: 0.02rem;
   /* Paint an image in the border */
-  background-image: 
-  linear-gradient(#000, #000),
-  conic-gradient(
+  background-image: conic-gradient(
     from var(--angle),
     #fff 0deg 90deg,
     #787878 90deg 180deg,
     #fff 180deg 270deg,
     #787878 270deg 360deg
   );
-  
   background-origin: border-box;
   background-clip: padding-box, border-box;
   animation: rotate 0.9s ease infinite;
 
+  position: relative;
+
+  &::after {
+    content: "";
+    display: block;
+    width: 95%;
+    height: 95%;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 0.02rem;
+    background-color: #000;
+  }
+
   &.inactive {
     animation: none;
     border-color: #787878;
-    border-image: unset;
+    background-image: unset;
+  }
+
+  &--heart {
+    clip-path: shape(
+      from 50% 91%,
+      line to 90% 50%,
+      arc to 50% 9% of 1%,
+      arc to 10% 50% of 1%
+    );
+    &::after {
+      clip-path: shape(
+        from 50% 91%,
+        line to 90% 50%,
+        arc to 50% 9% of 1%,
+        arc to 10% 50% of 1%
+      );
+    }
   }
 }
 </style>
